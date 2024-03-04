3e6d7e5ff861be9600814ede6bfae55654a0271d
Mateo Elias <oscarmateoelias@gmail.com>
2024-03-03 21:43:58 -0600

    Modificaciones en funciones y más

--- controllers/HomeController.php
+++ controllers/HomeController.php
@@ -2,12 +2,20 @@
 
 namespace Controllers;
 
+use Model\GerenteGeneral;
+use Model\GerenteSucursal;
+use Model\Usuario;
 use MVC\Router;
 
 class HomeController
 {
     public static function index(Router $router)
     {
+
+        $usuario = new Usuario([]);
+
+        debuguear($usuario);
+
         $router->render("homepage", [
             'titulo' => 'HomePage',
         ]);
--- includes/funciones.php
+++ includes/funciones.php
@@ -9,6 +9,20 @@ function debuguear($variable): string
     exit;
 }
 
+// Función para debugear de manera más legible
+function debuguear_json($variable): string
+{
+    // Codificar la variable como JSON
+    $json = json_encode($variable, JSON_PRETTY_PRINT);
+
+    // Mostrar el JSON dentro de la etiqueta <pre>
+    echo '<pre><code>' . htmlspecialchars($json) . '</code></pre>';
+
+    // Terminar la ejecución del script
+    exit;
+}
+
+
 // Limpiar Salida de HTML
 function s($html): string
 {
